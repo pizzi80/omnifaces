2bf249db077c5fa4934d24dc1c84132135cab971
pizzi80 <paolo@given2.com>
2025-02-10 19:40:55 +0100

    SocketEvent cosmetics
    Signed-off-by: pizzi80 <paolo@given2.com>

--- src/main/java/org/omnifaces/cdi/push/SocketEvent.java
+++ src/main/java/org/omnifaces/cdi/push/SocketEvent.java
@@ -139,7 +139,7 @@ public final class SocketEvent implements Serializable {
          * The literal of {@link Opened}.
          * @since 3.2
          */
-        public static final AnnotationLiteral<Opened> LITERAL = new AnnotationLiteral<>() {
+        AnnotationLiteral<Opened> LITERAL = new AnnotationLiteral<>() {
             private static final long serialVersionUID = 1L;
         };
     }
@@ -163,7 +163,7 @@ public final class SocketEvent implements Serializable {
         /**
          * The literal of {@link Switched}.
          */
-        public static final AnnotationLiteral<Switched> LITERAL = new AnnotationLiteral<>() {
+        AnnotationLiteral<Switched> LITERAL = new AnnotationLiteral<>() {
             private static final long serialVersionUID = 1L;
         };
     }
@@ -188,7 +188,7 @@ public final class SocketEvent implements Serializable {
          * The literal of {@link Closed}.
          * @since 3.2
          */
-        public static final AnnotationLiteral<Closed> LITERAL = new AnnotationLiteral<>() {
+        AnnotationLiteral<Closed> LITERAL = new AnnotationLiteral<>() {
             private static final long serialVersionUID = 1L;
         };
     }
